2ecac4d098ab56c173e612954c34a57b6e1fdb93
athidi21athy <athidi21athy@gmail.com>
2018-10-14 12:57:40 +0100

    added integration test for create games endpoint

---
+++ src/main/com/athidi21athy/kalahapi/domain/Game.java
@@ -0,0 +1,14 @@
+package com.athidi21athy.kalahapi.domain;
+
+public class Game {
+    private Integer id;
+    private String uri;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getUri() {
+        return this.uri;
+    }
+}
---
+++ src/test/com/athidi21athy/kalahapi/IntegrationTest.java
@@ -0,0 +1,35 @@
+package com.athidi21athy.kalahapi;
+
+import com.athidi21athy.kalahapi.domain.Game;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testName() throws Exception {
+        // arrange
+
+        // act
+        ResponseEntity<Game> response = restTemplate.postForEntity("/games", null, Game.class);
+
+        // assert
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        Assertions.assertThat(response.getBody().getId()).isInstanceOfAny(Integer.class);
+        Assertions.assertThat(response.getBody().getUri()).isEqualTo("/games/" + response.getBody().getId());
+    }
+}
