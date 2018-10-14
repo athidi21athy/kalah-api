ba0e7f7e7620c79ee3e1bb18dd1414a3dc10b613
athidi21athy <athidi21athy@gmail.com>
2018-10-14 12:03:31 +0100

    adding spring initializr contents

---
+++ src/main/java/com/athidi21athy/kalahapi/KalahApiApplication.java
@@ -0,0 +1,12 @@
+package com.athidi21athy.kalahapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalahApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalahApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/athidi21athy/kalahapi/KalahApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.athidi21athy.kalahapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KalahApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
